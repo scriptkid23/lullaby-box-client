a8d8129baabc01514d7c23be2d812628678d6d4d
HoanDo <sieunhannhenkutesn@gmail.com>
2021-06-26 09:53:44 +0700

    fix: fix bug join room

--- src/screens/components/join-room.component.js
+++ src/screens/components/join-room.component.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { Modal, Button, Form, InputGroup } from "react-bootstrap";
+import {Alert} from 'reactstrap'
 import { useForm } from "react-hook-form";
 import { v4 as uuidv4 } from "uuid";
 
@@ -8,36 +9,65 @@ import { SocketContext } from "../../context/socket.context";
 import axios from "axios";
 import { baseUrl } from "../../constants";
 import SelectAvatar from "./select-avatar.component";
+const AlertJoinRoom = (props) => {
+
+  return (
+    <Alert color="danger" isOpen={props.visible}>
+      {props.alert}
+    </Alert>
+  );
+}
+
 export default function JoinRoomComponent() {
   const [show, setShow] = useState(false);
-  const {actions} = React.useContext(SocketContext);
+  const [visible, setVisible] = useState(false);
+  const [alert, setAlert] = useState("");
+  const { actions } = React.useContext(SocketContext);
   const { register, handleSubmit } = useForm();
   const history = useHistory();
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
-  
-  const onSubmit = (data) => {
-    if(localStorage.getItem("avatar").length > 0){
-      const userId  =uuidv4();
-      axios.post(baseUrl+'/room/join',{
+
+  const onSubmit = async (data) => {
+    let isExist = await axios.get(baseUrl + "/room/check/" + data.roomId);
+    console.log(isExist.data)
+    if (!isExist.data) {
+      setVisible(true);
+      setAlert("Room not exist 😅")
+      return;
+    }
+    if (
+      localStorage.getItem("avatar") &&
+      localStorage.getItem("avatar").length > 0
+    ) {
+      const userId = uuidv4();
+      axios.post(baseUrl + "/room/join", {
         roomId: data.roomId,
-        participant:{
+        participant: {
           userId: userId,
           name: data.name,
           avatar: localStorage.getItem("avatar"),
-        }
-      })
-      localStorage.setItem("userId",userId);
+        },
+      });
+      localStorage.setItem("userId", userId);
       localStorage.setItem("name", data.name);
       // localStorage.setItem("avatar", data.avatar);
-      localStorage.setItem("roomId",data.roomId);
+      localStorage.setItem("roomId", data.roomId);
       history.push("/room/"+data.roomId);
+      setVisible(false);
+      setAlert("");
+    }else{
+      setVisible(true);
+      setAlert('Please select avatar 😉')
     }
-  
+
   };
   return (
     <>
-      <button className="btn btn-gradient-random btn-block" onClick={handleShow}>
+      <button
+        className="btn btn-gradient-random btn-block"
+        onClick={handleShow}
+      >
         Join Room
       </button>
       <Modal
@@ -70,9 +100,13 @@ export default function JoinRoomComponent() {
             </Form.Group>
             <Form.Group>
               <Form.Label>Avatar</Form.Label>
-              <SelectAvatar/>
+              <SelectAvatar />
             </Form.Group>
           </Form>
+          <AlertJoinRoom
+            visible={visible}
+            alert={alert}
+          />
         </Modal.Body>
         <Modal.Footer>
           <Button variant="secondary" onClick={handleClose}>
